5f35376b0208bdfceee6c8dbb1bf54a50938fa38
Taras Nesterenko <tnestere@gmail.com>
2024-03-31 15:02:57 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+import Results from './components/Results';
+import UserInput from './components/UserInput';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <UserInput />
+      <Results />
+    </>
+  );
+}
+
+export default App;
